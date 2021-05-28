7f614ec5ccf1b64f44d57aaf9782659f90977714
Chamara-Wijepala <mr.worf777@gmail.com>
2021-05-28 18:29:41 +0530

    Add function that takes data from form and creates 2 players objects

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-const player = (symbol) => {
-
-    return {};
+const playerFactory = (name, symbol) => {
+    return {
+        name,
+        symbol,
+    };
 };
 
 const gameBoard = (() => {
     let _board = ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'O', 'X'];
 
     const createField = () => {
-        const field = document.querySelector('.field');
         for (i = 0; i < _board.length; i++) {
-            let square = field.appendChild(document.createElement('button'));
+            let square = document.querySelector('.field').appendChild(document.createElement('button'));
             square.className = 'square';
             square.innerText = _board[i];
             square.dataset.ID = i;
         };
     };
 
-    return {createField};
+    const setField = (symbol) => {
+
+    };
+
+    return {
+        createField,
+        setField,
+    };
 })();
 
+const game = (() => {
+    gameBoard.createField();
 
+    const processForm = () => {
+        const formData = document.getElementById('form')
 
-gameBoard.createField();
+        const player1 = playerFactory(formData[0].value, formData[1].value);
+        const player2 = playerFactory(formData[2].value, formData[3].value);
+
+        return {
+            player1,
+            player2,
+        };
+    };
+    //document.querySelector('.field').addEventListener('click', add function that calls setfield and passes it player's symbol)
+
+    return {
+        processForm,
+    };
+})();
